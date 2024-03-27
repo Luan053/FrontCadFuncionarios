30f0a0043c8cd7723a1d0b454fa4502e529d1451
Luan053 <luanrschulzz@gmail.com>
2024-03-27 16:00:21 -0300

    feat: base da tela home

---
+++ src/components/pagesComponents/CardHome.tsx
@@ -0,0 +1,42 @@
+import { Pencil1Icon, TrashIcon } from "@radix-ui/react-icons";
+import { Button } from "../ui/button";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardFooter,
+  CardContent,
+} from "../ui/card";
+
+import AnimeVPNG from "@/assets/AnimeVPNG.png";
+
+export const CardHome = () => {
+  return (
+    <Card className="flex justify-between w-full max-w-md">
+      <div className="w-3/4 flex flex-col justify-between">
+        <CardHeader>
+          <CardTitle className="text-2xl font-bold tracking-tighter">
+            Nome do Funcionário
+          </CardTitle>
+          <CardDescription className="text-md">Role</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Button className="bg-red-500">
+            <TrashIcon className="h-6 w-6" />
+          </Button>
+          <Button className="bg-amber-400 ml-2">
+            <Pencil1Icon className="h-6 w-6" />
+          </Button>
+        </CardContent>
+      </div>
+      <CardFooter className="w-1/4 flex p-2">
+        <img
+          src={AnimeVPNG}
+          alt="Funcionário"
+          className="w-full h-5/6 rounded-md object-cover"
+        />
+      </CardFooter>
+    </Card>
+  );
+};
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,42 @@
+import { CardHome } from "@/components/pagesComponents/CardHome";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { ExitIcon, MagnifyingGlassIcon, PlusIcon } from "@radix-ui/react-icons";
+
 export const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="h-screen w-screen flex flex-col gap-2 bg-slate-200">
+      <header className="w-full h-[76px] bg-slate-800 flex items-center justify-between p-4">
+        <h1 className="text-white text-2xl font-bold tracking-tighter">
+          Logo Sistema
+        </h1>
+        <div className="flex gap-2">
+          <div className="flex gap-1">
+            <Input
+              placeholder="Buscar Funcionário"
+              className="text-white border-none bg-slate-700"
+              required
+            />
+            <Button
+              type="submit"
+              className="text-white bg-sky-500 p-2 rounded-sm"
+            >
+              <MagnifyingGlassIcon className="h-8 w-8" />
+            </Button>
+          </div>
+          <Button className="text-white bg-emerald-500 p-2 rounded-sm">
+            <PlusIcon className="h-8 w-8" />
+          </Button>
+          <Button className="text-white bg-red-500 p-2 rounded-sm ml-4">
+            <ExitIcon className="h-8 w-8" />
+          </Button>
+        </div>
+      </header>
+      <div className="flex p-4 gap-4 flex-wrap">
+        {Array.from({ length: 6 }).map((_, index) => (
+          <CardHome key={index} />
+        ))}
+      </div>
     </div>
   );
 };
